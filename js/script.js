9ba8c097ab71cdbcaff4d4fa50f3bc53cd0f3e15
geovanaclira <geovana.liira@gmail.com>
2025-08-26 15:46:26 -0300

    matriz criada

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const perguntasERespostas = [
+    {
+        pergunta: "Qual estrutura de repetição utilizamos quando sabemos a quantidade exata das repetições no código?",
+        alternativas: ["A) While", "B) Do-While", "C) For"],
+        respostaCorreta: "C"
+    },
+
+    {
+        pergunta: "Em JavaScript, qual a principal diferença entre 'let' e 'const' para declarar variáveis?",
+        alternativas: ["A)'let' é para strings e 'const' é para números", "B) 'const' permite reatribuição e 'let' não", "C)'let' permite reatribuição e 'const' não"],
+        respostaCorreta: "C"
+    },
+
+    {
+        pergunta: "Qual operador lógico é usado para representar 'OU' (OR)?",
+        alternativas: ["A) &&", "B) ||", "C) != "],
+        respostaCorreta: "B"
+    },
+
+    {
+       pergunta: "Qual estrutura é usada para testar vários valores da mesma variável?",
+       alternativas: ["A) Switch", "B) While", "C) Do-While"],
+       respostaCorreta: "A" 
+    }
+];
+
